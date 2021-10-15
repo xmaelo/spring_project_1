361fcf5603093baae18585f713db59b2810740b3
ismael <ismael.ngouate@gmail.com>
2021-10-15 11:02:13 +0100

    stated repos

---
+++ rest/src/main/java/com/rest_api/rest/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.rest_api.rest.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rest_api.rest.models.Employee;
+
+@Repository
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+}
